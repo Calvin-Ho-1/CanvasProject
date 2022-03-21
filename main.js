2c60d94c4caac0c32d5384d0873a54519f63bbf8
Ninjabot10000 <ninjabot10000@gmail.com>
2022-03-20 22:26:33 -0700

    added and removed comment lines

--- main.js
+++ main.js
@@ -234,9 +234,11 @@ var update = function (modifier) {
 		pikachu.y -= pikachu.speed * modifier;
 		if (pikachu.y < ( 0) ) {
 			pikachu.y = 0; //reason to make this 0 instead of the 32 pixel border is because of pikachu's image is small (and to not overlap with the "ketchups found" text)	
+			
 			//adds a bump sound
 			//!!!! This will ONLY play if the player hits the wall by TAPPING IT!!! HOLDING THE MOVE KEY WILL NOT PLAY THE BUMP SOUND! 
-			//!!! This works, BUT it WILL pop up that it was interrupted by a new load request (in the console log). This does NOT affect the gameplay or causes any problems with the game.
+			//!!! This works, but it pops up that the play request was interrupted (in the console log).
+			//This does NOT affect the gameplay or causes any problems with the game (including the sound effect). 
 			soundEfx.src = soundBump;
 			soundEfx.play();
 		}
@@ -250,9 +252,11 @@ var update = function (modifier) {
 		pikachu.y += pikachu.speed * modifier;
 		if (pikachu.y > (1000 - ( 160) )) {
 			pikachu.y = 1000 	 -160; //adjusted
+			
 			//adds a bump sound
 			//!!!! This will ONLY play if the player hits the wall by TAPPING IT!!! HOLDING THE MOVE KEY WILL NOT PLAY THE BUMP SOUND! 
-			//!!! This works, BUT it WILL pop up that it was interrupted by a new load request (in the console log). This does NOT affect the gameplay or causes any problems with the game.
+			//!!! This works, but it pops up that the play request was interrupted (in the console log).
+			//This does NOT affect the gameplay or causes any problems with the game (including the sound effect). 
 			soundEfx.src = soundBump;
 			soundEfx.play();
 		}
@@ -265,9 +269,11 @@ var update = function (modifier) {
 		pikachu.x -= pikachu.speed * modifier;
 		if (pikachu.x < ( 5) ) {
 			pikachu.x = 5; //adjusted
+			
 			//adds a bump sound
 			//!!!! This will ONLY play if the player hits the wall by TAPPING IT!!! HOLDING THE MOVE KEY WILL NOT PLAY THE BUMP SOUND! 
-			//!!! This works, BUT it WILL pop up that it was interrupted by a new load request (in the console log). This does NOT affect the gameplay or causes any problems with the game.
+			//!!! This works, but it pops up that the play request was interrupted (in the console log).
+			//This does NOT affect the gameplay or causes any problems with the game (including the sound effect). 
 			soundEfx.src = soundBump;
 			soundEfx.play();
 		}
@@ -282,7 +288,8 @@ var update = function (modifier) {
 			pikachu.x = 1000 - (32 +100 ); //adjusted
 			//Adds a bump sound
 			//!!!! This will ONLY play if the player hits the wall by TAPPING IT!!! HOLDING THE MOVE KEY WILL NOT PLAY THE BUMP SOUND! 
-			//!!! This works, BUT it WILL pop up that it was interrupted by a new load request (in the console log). This does NOT affect the gameplay or causes any problems with the game.
+			//!!! This works, but it pops up that the play request was interrupted (in the console log).
+			//This does NOT affect the gameplay or causes any problems with the game (including the sound effect). 
 			soundEfx.src = soundBump;
 			soundEfx.play();
 		}
